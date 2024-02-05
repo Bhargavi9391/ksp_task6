93976d28790054f9afce943c017195fe07d00c8b
Bhargavi9391 <157037629+Bhargavi9391@users.noreply.github.com>
2024-02-05 19:33:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let num=5;
+if(num<10){
+    console.log("hello");
+}
+
+let numbers;
+for(numbers=0;numbers<=10;numbers++){
+    console.log("hello GGoogle");
+
+}
+
+
+#Database Coding
+
+SELECT*FROM products;
+SELECT*FROM contactNames;
+SELECT ContactName FROM Customers;
+SELECT *FROM Customers;
+WHERE City LIKE 'm';//in the data the name starts with m it will shown in the op;
+SELECT * FROM Customers;
+WHERE Country ='mexico';
+AND City ='berlin';
